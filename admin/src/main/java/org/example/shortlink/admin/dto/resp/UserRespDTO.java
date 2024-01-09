24a7670215b9b1a2a694cfaae32444c0479c7bc3
ruisen <484539372@qq.com>
2024-01-09 22:55:52 +0800

    新增 用户铭感信息接口返回脱敏

--- admin/src/main/java/org/example/shortlink/admin/dto/resp/UserRespDTO.java
+++ admin/src/main/java/org/example/shortlink/admin/dto/resp/UserRespDTO.java
@@ -1,6 +1,8 @@
 package org.example.shortlink.admin.dto.resp;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
+import org.example.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
 
 /**
  * 用户返回参数响应
@@ -27,6 +29,7 @@ public class UserRespDTO {
     /**
      * 手机号
      */
+    @JsonSerialize(using = PhoneDesensitizationSerializer.class) // 序列化时脱敏
     private String phone;
 
     /**
