33e1ef0ef6280f7e2d7a717fb541e128d9bb27c3
ruisen <484539372@qq.com>
2024-01-22 12:51:44 +0800

    后管系统新增 新增保存短链接分组功能

--- admin/src/main/java/org/example/shortlink/admin/dao/entity/UserDO.java
+++ admin/src/main/java/org/example/shortlink/admin/dao/entity/UserDO.java
@@ -1,15 +1,12 @@
 package org.example.shortlink.admin.dao.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
-
-import java.util.Date;
+import org.example.shortlink.admin.common.database.BaseDO;
 
 @Data // @Data是Lombok的一个注解，用于同时生成getter、setter、toString、equals和hashcode方法。可以简化Java类的代码编写，提高开发效率。
 @TableName("t_user") // @TableName注解用于在Java中指定函数操作的目标表是哪个，方便在代码中进行数据库操作时指定表名。
-public class UserDO {
+public class UserDO extends BaseDO {
 
 
     /**
@@ -47,22 +44,5 @@ public class UserDO {
      */
     private Long deletionTime;
 
-    /**
-     * 创建时间
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Date createTime;
-
-    /**
-     * 修改时间
-     */
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Date updateTime;
-
-    /**
-     * 删除标识 0：未删除 1：已删除
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private int delFlag;
 
 }
