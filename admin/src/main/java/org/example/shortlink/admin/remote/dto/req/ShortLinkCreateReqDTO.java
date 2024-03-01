7939dfcf9345a8ebfbb190ece5c361baad0db028
ruisen <484539372@qq.com>
2024-03-01 21:39:24 +0800

    短链接系统：修改短链接创建实体属性validDate，加上日期格式化注解

--- admin/src/main/java/org/example/shortlink/admin/remote/dto/req/ShortLinkCreateReqDTO.java
+++ admin/src/main/java/org/example/shortlink/admin/remote/dto/req/ShortLinkCreateReqDTO.java
@@ -1,5 +1,6 @@
 package org.example.shortlink.admin.remote.dto.req;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -38,6 +39,11 @@ public class ShortLinkCreateReqDTO {
     /**
      * 有效期
      */
+    // JsonFormat:用于指定JSON序列化和反序列化时日期格式的格式化规则。
+    // 其中，pattern属性指定了日期格式的模式
+    // timezone属性指定了时区。
+    // 在序列化和反序列化日期对象时，会根据指定的格式化规则进行转换。
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date validDate;
 
     /**
